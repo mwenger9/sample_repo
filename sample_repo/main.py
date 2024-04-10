99d9c2cbd9e4537c597bef7bc737de58d865765f
Marlon Wenger <74914160+mwenger9@users.noreply.github.com>
2024-04-10 14:37:07 +0200

    dsq

--- sample_repo/main.py
+++ sample_repo/main.py
@@ -1,4 +1,5 @@
-
+import numpy as np
 
 def hello():
+    x = np.array([])
     print("hello")
--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version=0.1,
     packages=find_packages(),
     install_requires=[
-
+        "numpy>=1.11.1"
     ],
 )
